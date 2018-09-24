7ddaf1156960a848965d7d7f85545d4029c1fe2c
AS3-2-16 <AS3-2-16@uni.lan>
2018-09-24 13:23:16 +0200

    array

--- Array.php
+++ Array.php
@@ -2,6 +2,11 @@
 
 $cars = array("Volvo","BMW","Toyota");
 
+for ($x = 0; $x < $cars.lenght; $++) {
+    echo $cars[$x]."<br>";
+}
+
+echo "<br><br><br>";
 
 foreach($cars as $car) {
     echo $car."<br>";
