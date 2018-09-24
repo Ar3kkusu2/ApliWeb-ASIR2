c0eee7234c46cc45530e9a1c47c3310d09d34eef
AS3-2-16 <AS3-2-16@uni.lan>
2018-09-24 13:28:06 +0200

    array

--- Array.php
+++ Array.php
@@ -2,12 +2,12 @@
 
 $cars = array("Volvo","BMW","Toyota");
 
-for ($x = 0; $x < count($cars); $++) {
+for ($x = 0; $x < count($cars); $x++) {
     echo $cars[$x]."<br>";
 }
 
 echo "<br><br><br>";
 
-foreach($cars as $car) {
-    echo $car."<br>";
+foreach($cars as $coche) {
+    echo $coche."<br>";
 }
