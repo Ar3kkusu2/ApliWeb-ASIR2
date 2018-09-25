6b7bd632e122ac2c2868cb3b31607f8574872536
AS3-2-16 <AS3-2-16@uni.lan>
2018-09-25 09:00:10 +0200

    horario2

--- HorarioArray.php
+++ HorarioArray.php
@@ -4,7 +4,7 @@ $dia = date("N");
 $hora = date("G");
 
 
-$asignatura = array(
+$asig = array(
     "Bases de datos",  // --> 0
     "EIE",
     "Sistemas", 
@@ -15,11 +15,11 @@ $asignatura = array(
 
 
 $horario = array(
-    array($asignatura[0],$asignatura[1],$asignatura[2],$asignatura[2],$asignatura[3],$asignatura[3]); //Lunes --> 0
-    array($asignatura[3],$asignatura[4],$asignatura[5],$asignatura[5],$asignatura[0],$asignatura[0]); //Martes
-    array($asignatura[4],$asignatura[4],$asignatura[2],$asignatura[2],$asignatura[1],$asignatura[1]); //Miercoles
-    array($asignatura[5],$asignatura[5],$asignatura[2],$asignatura[2],$asignatura[3],$asignatura[3]); //Jueves
-    array($asignatura[4],$asignatura[4],$asignatura[5],$asignatura[5],$asignatura[6],$asignatura[6]); //Viernes --> 4
+    array($asig[0],$asig[1],$asig[2],$asig[2],$asig[3],$asig[3]); //Lunes --> 0
+    array($asig[3],$asig[4],$asig[5],$asig[5],$asig[0],$asig[0]); //Martes
+    array($asig[4],$asig[4],$asig[2],$asig[2],$asig[1],$asig[1]); //Miercoles
+    array($asig[5],$asig[5],$asig[2],$asig[2],$asig[3],$asig[3]); //Jueves
+    array($asig[4],$asig[4],$asig[5],$asig[5],$asig[6],$asig[6]); //Viernes --> 4
 )
 
 if($dia == 6 || $dia == 7 || $hora < 8 || $hora > 14) {
