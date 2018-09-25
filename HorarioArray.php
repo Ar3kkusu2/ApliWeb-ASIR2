0640c8ddd3d6ebc94c7cec84ad1d24d6e5f7a0c2
AS3-2-16 <AS3-2-16@uni.lan>
2018-09-25 09:06:47 +0200

    horario2

--- HorarioArray.php
+++ HorarioArray.php
@@ -4,11 +4,11 @@ $dia = date("N");
 $hora = date("G");
 
 $horario = array(
-    array("Bases de datos","EIE","Sistemas","Sistemas","Aplicaciones","Aplicaciones"); //Lunes --> 0
-    array("Aplicaciones","Seguridad","Redes","Redes","Bases de datos","Bases de datos"); //Martes
-    array("Seguridad","Seguridad","Sistemas","Sistemas","EIE","EIE"); //Miercoles
-    array("Redes","Redes","Sistemas","Sistemas","Aplicaciones","Aplicaciones"); //Jueves
-    array("Seguridad","Seguridad","Redes","Redes","Ingles","Ingles"); //Viernes --> 4
+    array("Bases de datos","EIE","Sistemas","Sistemas","Aplicaciones","Aplicaciones"), //Lunes --> 0
+    array("Aplicaciones","Seguridad","Redes","Redes","Bases de datos","Bases de datos"), //Martes
+    array("Seguridad","Seguridad","Sistemas","Sistemas","EIE","EIE"), //Miercoles
+    array("Redes","Redes","Sistemas","Sistemas","Aplicaciones","Aplicaciones"), //Jueves
+    array("Seguridad","Seguridad","Redes","Redes","Ingles","Ingles") //Viernes --> 4
 )
 
 if($dia == 6 || $dia == 7 || $hora < 8 || $hora > 14) {
