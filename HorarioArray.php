6c303489fe38ed26eea96adcb3c14dcd14c59118
AS3-2-16 <AS3-2-16@uni.lan>
2018-09-25 09:05:45 +0200

    horario2

--- HorarioArray.php
+++ HorarioArray.php
@@ -3,27 +3,16 @@
 $dia = date("N");
 $hora = date("G");
 
-
-$asig = array(
-    "Bases de datos",  // --> 0
-    "EIE",
-    "Sistemas", 
-    "Aplicaciones",  
-    "Seguridad",  
-    "Redes",  
-    "Ingles"); // --> 6
-
-
 $horario = array(
-    array($asig[0],$asig[1],$asig[2],$asig[2],$asig[3],$asig[3]); //Lunes --> 0
-    array($asig[3],$asig[4],$asig[5],$asig[5],$asig[0],$asig[0]); //Martes
-    array($asig[4],$asig[4],$asig[2],$asig[2],$asig[1],$asig[1]); //Miercoles
-    array($asig[5],$asig[5],$asig[2],$asig[2],$asig[3],$asig[3]); //Jueves
-    array($asig[4],$asig[4],$asig[5],$asig[5],$asig[6],$asig[6]); //Viernes --> 4
+    array("Bases de datos","EIE","Sistemas","Sistemas","Aplicaciones","Aplicaciones"); //Lunes --> 0
+    array("Aplicaciones","Seguridad","Redes","Redes","Bases de datos","Bases de datos"); //Martes
+    array("Seguridad","Seguridad","Sistemas","Sistemas","EIE","EIE"); //Miercoles
+    array("Redes","Redes","Sistemas","Sistemas","Aplicaciones","Aplicaciones"); //Jueves
+    array("Seguridad","Seguridad","Redes","Redes","Ingles","Ingles"); //Viernes --> 4
 )
 
-/*if($dia == 6 || $dia == 7 || $hora < 8 || $hora > 14) {
+if($dia == 6 || $dia == 7 || $hora < 8 || $hora > 14) {
     echo "Fiesta";
-}*/
+}
 
 echo = $horario[$dia-1][$hora-8];
