f84d9a8bb4e33210604ee39f50e9b719d046410e
AS3-2-16 <AS3-2-16@uni.lan>
2018-10-01 12:25:35 +0200

    horario

---
+++ HorarioTabla.php
@@ -0,0 +1,11 @@
+
+
+$horario = array(
+    array("Bases de datos","EIE","Sistemas","Sistemas","Aplicaciones","Aplicaciones"), //Lunes --> 0
+    array("Aplicaciones","Seguridad","Redes","Redes","Bases de datos","Bases de datos"), //Martes
+    array("Seguridad","Seguridad","Sistemas","Sistemas","EIE","EIE"), //Miercoles
+    array("Redes","Redes","Sistemas","Sistemas","Aplicaciones","Aplicaciones"), //Jueves
+    array("Seguridad","Seguridad","Redes","Redes","Ingles","Ingles") //Viernes --> 4
+);
+
+foreach()
