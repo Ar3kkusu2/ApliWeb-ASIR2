f5382fef69fbd64ba886f7502d1ab2e5e438fb79
AS3-2-16 <AS3-2-16@uni.lan>
2018-10-15 12:43:54 +0200

    login

--- Login/login.php
+++ Login/login.php
@@ -10,19 +10,19 @@ $conn = new mysqli("localhost:3307", "root", "", "apliweb");
 if (!$conn) {
      die("Conexion Fallida: ".mysqli_connect_error());
 }
-echo "Conexion Exitosa";
-echo "<br>";
-$query = "SELECT user from users WHERE User='$user' and Password='$pass'";
+//echo "Conexion Exitosa <br>";
 
-$result =mysql_query($conn, $query);
+$query = "SELECT User from users WHERE User='$user' AND Password='$pass'";
+
+$result = mysqli_query($conn, $query);
 
 if ($result) {
     $row = mysqli_fetch_array($result, MYSQLI_NUM);
-    echo $row[0];
+    echo $row[0]." ";
 }
 
 if ($row) {
     echo "Accesso Concendido";
 }else {
-    echo "Accesso Denegado",
+    echo "Accesso Denegado";
 }
