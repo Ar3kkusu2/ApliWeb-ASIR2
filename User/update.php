2caf1c96700c881e53fbe7f37241a3aa6a6bf790
AS3-2-16 <AS3-2-16@uni.lan>
2018-10-22 13:11:16 +0200

    login

--- User/update.php
+++ User/update.php
@@ -2,15 +2,17 @@
 
 
 $user = $_POST["user"];
-
+echo $user;
 ?>
 <form action="update2.php" method="POST">
 User: <?php echo $user?>      
-<input type='hidden' name='user' value="<?php $user ?>"><br>
+<input type='hidden' name='user' value="<?php echo $user?>"><br>
 Password:       
 <input type='password' name='pass'><br>
 <input type='submit'  value='Update'>
 </form>
+
+
 
 
 
